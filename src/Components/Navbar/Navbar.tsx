c022f0305b02d86dc4f0afc46bde80e0cc6ef79a
suraj-web <shakyasuraj595@gamil.com>
2025-05-01 18:02:03 +0530

    add

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -3,11 +3,11 @@ import Link from "next/link";
 export default function Navbar() {
     return (
         <nav className="flex justify-between z-50 items-start fixed top-0 w-full h-[90px] px-[80px] text-black">
-            <Link href={"/"} className="text-[25px] pacifico-regular flex items-center h-[90%] rounded-b-2xl px-6 gap-2 bg-[#DA392B] text-white">
+            <Link href={"/"} className="text-[25px] pacifico-regular flex items-center h-[80%] rounded-b-2xl px-6 gap-2 bg-[#DA392B] text-white">
                 Tripo Games
             </Link>
 
-            <ul className="flex h-[90%] w-[50%] rounded-b-2xl items-center justify-center bg-white text-xl font-sans z-99">
+            <ul className="flex h-[80%] w-[50%] rounded-b-2xl items-center justify-center bg-white text-xl font-sans z-99">
                 {Navbardata1.map((item, index) => (
                     <li key={index} className="w-[40%] h-full">
                         <Link href={item.href} className={`text-2xl font-serif h-full flex justify-center items-center w-full transition-all ease-in duration-300 hover:bg-[#DA392B] hover:text-white ${index === 0 ? "rounded-bl-2xl" : ""}`}>
